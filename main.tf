d6b31e18dc6e9b710b269b05accc9b4be8561aab
Remo Mattei <remo@mattei.org>
2021-10-30 12:04:11 -0700

    ssh key

--- main.tf
+++ main.tf
@@ -14,14 +14,15 @@ resource "aws_instance" "centos-7-newbox" {
   }
   
   provisioner "remote-exec" {
-    #connection {
-    #  type  = "ssh"
-    #  user  = "centos"
-    host = "${join(",", aws_instance.centos-7[count.index].id)}"
-    #host  = self.public_ip
-    #host  = "${element(aws_instance.centos-7.*.public_ip, count.index)}"
-    #  agent = "true"
-    #}
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      public_key = var.ssh_key
+    #host         = "${join(",", aws_instance.centos-7[count.index].id)}"
+    #host         = self.public_ip
+    #host         = "${element(aws_instance.centos-7.*.public_ip, count.index)}"
+      agent       = false
+    }
     inline = [
      "sudo yum install -y yum-utils",
      "sudo yum-config-manager  --add-repo  https://download.docker.com/linux/centos/docker-ce.repo",
