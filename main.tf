c261ff6aed73eae23a32f07955583d2eedbf7b23
Remo Mattei <remo@mattei.org>
2021-10-30 16:13:29 -0700

    adding ssh gen key

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "centos-7-newbox" {
   vpc_security_group_ids = ["${aws_security_group.remo_sg.id}"]
   instance_type               = var.image-size
   subnet_id                   = var.se_subnet_id
-  key_name                    = var.ssh_key
+  key_name                    = aws_key_pair.generated.key_name
   tags                        = {
     dept                      = var.department_name
     shutdown_policy           = var.shutdown_rules
@@ -13,20 +13,20 @@ resource "aws_instance" "centos-7-newbox" {
     Name                      = "${var.vm_name}-${count.index +1}"
   }
   
-  #provisioner "remote-exec" {
-  #  connection {
-  #    type        = "ssh"
-  #    user        = "centos"
-  #    private_key = var.p_ssh_key
-  #  #host         = "${join(",", aws_instance.centos-7[count.index].id)}"
-  #  host         = self.public_ip
-  #  #host         = "${element(aws_instance.centos-7.*.public_ip, count.index)}"
-  #    agent       = false
-  #  }
-  #  inline = [
-  #   "sudo yum install -y yum-utils",
-  #   "sudo yum-config-manager  --add-repo  https://download.docker.com/linux/centos/docker-ce.repo",
-  #   "sudo yum install -y docker"
-  #  ]
-  #}
+  provisioner "remote-exec" {
+    connection {
+      type  = "ssh"
+      user  = "centos"
+      private_key = tls_private_key.generated.public_key_openssh
+      #host = "${join(",", aws_instance.centos-7[count.index].id)}"
+      host  = self.public_ip
+      #host  = "${element(aws_instance.centos-7.*.public_ip, count.index)}"
+      agent = "true"
+    }
+    inline = [
+     "sudo yum install -y yum-utils",
+     "sudo yum-config-manager  --add-repo  https://download.docker.com/linux/centos/docker-ce.repo",
+     "sudo yum install -y docker"
+    ]
+  }
 }
